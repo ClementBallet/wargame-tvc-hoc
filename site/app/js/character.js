c250245a5f0b3b26f57624f609d021e84816f18b
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-05-22 16:15:33 +0200

    Correct rotate in CSS and continue on movement in character.js

--- site/app/js/character.js
+++ site/app/js/character.js
@@ -5,55 +5,68 @@ class character {
 
   moveCharacter() {
 
-    let step = -5;
+///////////////////////////////// VARIABLES /////////////////////////////////
+
+    let character1 = document.getElementByClassName(this.character)
+    const step = 5;
+    let character1PositionX =
+    let character1PositionY =
+    let character2PositionX =
+    let character2PositionY =
+
+
+///////////////////////////////// ANIMATION /////////////////////////////////
 
     let playzone = document.querySelector('#playzone')
     let characterBody = document.createElement(div)
 
     playzone.appendChild(characterBody)
+    playzone.getElementByTagName('div').classList.add(this.character)
 
     if (moveLeft == true || moveUp == true || moveRight == true || moveDown == true) {
-      playzone.getElementByTagName("div").classList.remove(this.character + "Idle")
-      playzone.getElementByTagName("div").classList.add(this.character + "Move")
+      playzone.getElementByClassName(this.character).classList.remove(this.character + "Idle")
+      playzone.getElementByClassName(this.character).classList.add(this.character + "Move")
     }
 
     if (moveLeft == false || moveUp == false || moveRight == false || moveDown == false) {
-      playzone.getElementByTagName("div").classList.remove(this.character + "Move")
-      playzone.getElementByTagName("div").classList.add(this.character + "Idle")
+      playzone.getElementByClassName(this.character).classList.remove(this.character + "Move")
+      playzone.getElementByClassName(this.character).classList.add(this.character + "Idle")
     }
 
-//player1
+//////////////////////////// MOVEMENT & ROTATION ////////////////////////////
+
+  //player1
     if (moveUp == true) {
-      document.querySelector("." + this.character + "Move").classList.add(rotate180)
+      document.getElementByClassName(this.character).classList.add(rotate180)
     }
 
     if (moveLeft == true) {
-      document.querySelector("." + this.character + "Move").classList.add(rotate90)
+      document.getElementByClassName(this.character).classList.add(rotate90)
     }
 
     if (moveRight == true) {
-      document.querySelector("." + this.character + "Move").classList.add(rotateLess90)
+      document.getElementByClassName(this.character).classList.add(rotateLess90)
     }
 
     if (moveDown == true) {
-      document.querySelector("." + this.character + "Move").classList.remove("rotate90", "rotate180", "rotateLess90")
+      document.getElementByClassName(this.character).classList.remove("rotate90", "rotate180", "rotateLess90")
     }
 
   //player2
     if (moveUp == true) {
-      document.querySelector("." + this.character + "Move").classList.remove("rotate90", "rotate180", "rotateLess90")
+      document.getElementByClassName(this.character).classList.remove("rotate90", "rotate180", "rotateLess90")
     }
 
     if (moveLeft == true) {
-      document.querySelector("." + this.character + "Move").classList.add(rotateLess90)
+      document.getElementByClassName(this.character).classList.add(rotateLess90)
     }
 
     if (moveRight == true) {
-      document.querySelector("." + this.character + "Move").classList.add(rotate90)
+      document.getElementByClassName(this.character).classList.add(rotate90)
     }
 
     if (moveDown == true) {
-      document.querySelector("." + this.character + "Move").classList.add(rotate180)
+      document.getElementByClassName(this.character).classList.add(rotate180)
     }
   }
 }
