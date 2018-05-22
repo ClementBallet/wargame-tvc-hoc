437eb0fd9ee306b2590028905498bec60517821b
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-05-22 15:31:13 +0200

    adding player 1 & 2 rotation

--- site/app/js/character.js
+++ site/app/js/character.js
@@ -5,13 +5,21 @@ class character {
 
   moveCharacter() {
 
+    let step = -5;
+
     let playzone = document.querySelector('#playzone')
     let characterBody = document.createElement(div)
 
     playzone.appendChild(characterBody)
 
     if (moveLeft == true || moveUp == true || moveRight == true || moveDown == true) {
+      playzone.getElementByTagName("div").classList.remove(this.character + "Idle")
       playzone.getElementByTagName("div").classList.add(this.character + "Move")
+    }
+
+    if (moveLeft == false || moveUp == false || moveRight == false || moveDown == false) {
+      playzone.getElementByTagName("div").classList.remove(this.character + "Move")
+      playzone.getElementByTagName("div").classList.add(this.character + "Idle")
     }
 
 //player1
@@ -25,6 +33,27 @@ class character {
 
     if (moveRight == true) {
       document.querySelector("." + this.character + "Move").classList.add(rotateLess90)
+    }
+
+    if (moveDown == true) {
+      document.querySelector("." + this.character + "Move").classList.remove("rotate90", "rotate180", "rotateLess90")
+    }
+
+  //player2
+    if (moveUp == true) {
+      document.querySelector("." + this.character + "Move").classList.remove("rotate90", "rotate180", "rotateLess90")
+    }
+
+    if (moveLeft == true) {
+      document.querySelector("." + this.character + "Move").classList.add(rotateLess90)
+    }
+
+    if (moveRight == true) {
+      document.querySelector("." + this.character + "Move").classList.add(rotate90)
+    }
+
+    if (moveDown == true) {
+      document.querySelector("." + this.character + "Move").classList.add(rotate180)
     }
   }
 }
