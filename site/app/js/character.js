53e31d590f1b123011b692d7be83060388817011
micou1830 <38948344+micou1830@users.noreply.github.com>
2018-05-22 12:19:10 +0200

    add map.js and character.js / modified app.css

---
+++ site/app/js/character.js
@@ -0,0 +1,18 @@
+class character {
+  constructor() {
+
+  }
+
+  moveCharacter() {
+
+    let playzone = document.querySelector('#playzone')
+    let this.character = document.createElement(div)
+
+    playzone.appendChild(this.character)
+
+    if (moveLeft == true) {
+      classList.add("hitmanMove")
+      classList.add
+    }
+  }
+}
